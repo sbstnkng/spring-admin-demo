4f255d423b32a11d4ca640a53b52e4b07cb6db3a
sbstnkng <sebastian.koenig@scout24.com>
2021-08-05 13:13:28 +0200

    Add app test and update build pipeline

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import { Admin, Resource } from 'react-admin';
+import App from './App';
+
+xtest('renders App', () => {
+  render(<App />);
+  //TODO: do some testing here
+});
