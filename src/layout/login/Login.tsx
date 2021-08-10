515bf7cf2f4f9ec60b0ac3a3e6c75fa18c65fe3c
sbstnkng <sebastian.koenig@scout24.com>
2021-08-10 16:16:54 +0200

    Add hint for Login

--- src/layout/login/Login.tsx
+++ src/layout/login/Login.tsx
@@ -1,5 +1,23 @@
-import { Login as RALogin } from 'react-admin';
+import { Login as RaLogin, LoginForm } from 'react-admin';
+import { makeStyles } from '@material-ui/core/styles';
 
-export const Login = () => (
-  <RALogin backgroundImage="https://source.unsplash.com/1600x900/?landscape" />
-);
+const useStyles = makeStyles((theme) => ({
+  hint: {
+    marginTop: '1em',
+    display: 'flex',
+    justifyContent: 'center',
+    color: theme.palette.grey[500],
+  },
+}));
+
+export const Login = (props: any) => {
+  const classes = useStyles();
+  return (
+    <RaLogin backgroundImage="https://source.unsplash.com/1600x900/?landscape">
+      <div>
+        <div className={classes.hint}>Hint: demo / demo</div>
+        <LoginForm {...props} />
+      </div>
+    </RaLogin>
+  );
+};
