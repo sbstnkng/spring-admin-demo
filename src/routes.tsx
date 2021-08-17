a64a3dbf0f2ad1685905a67f2b125eeae46ce5c0
sbstnkng <koenigse@gmx.de>
2021-08-17 20:36:01 +0200

    Fix linter issue

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,4 +2,6 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import Profile from './profile/Profile';
 
-export default [<Route exact path="/profile" render={() => <Profile />} />];
+const routes = [<Route exact path="/profile" render={() => <Profile />} />];
+
+export default routes;
